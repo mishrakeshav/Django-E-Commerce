35f3369918a55f5767683560e28866d9ef910726
mishrakeshav <keshav.sm@somaiya.edu>
2020-12-22 17:05:42 +0530

    update guest cart

--- store/views.py
+++ store/views.py
@@ -16,16 +16,21 @@ def cart(request):
         customer = request.user.customer
         order , created = Order.objects.get_or_create(customer = customer, complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         try:
             cart = json.loads(request.COOKIES['cart'])
         except:
             cart = {}
-        print(cart)
+        
+
         items = []
         order = {'get_cart_items': 0, 'get_cart_total':0}
-
-    context = {'items':items, 'order':order}
+        cartItems = order.get('get_cart_items')
+        for i in cart:
+            cartItems += cart[i]['quantity']
+    
+    context = {'items':items, 'order':order, 'cartItems' : cartItems}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
